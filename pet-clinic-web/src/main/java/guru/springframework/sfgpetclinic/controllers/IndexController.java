cdc8dc068e40c197e409075ea10c7deaa52f72dd
alisenn <alisen4099@gmail.com>
2020-10-07 17:23:13 +0300

    images added. closes #17.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -3,6 +3,9 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by jt on 7/22/18.
+ */
 @Controller
 public class IndexController {
 
@@ -12,8 +15,8 @@ public class IndexController {
         return "index";
     }
 
-    /*@RequestMapping("/oups")
+    @RequestMapping("/oups")
     public String oupsHandler(){
         return "notimplemented";
-    }*/
+    }
 }
